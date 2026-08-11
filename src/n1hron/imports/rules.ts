b74119d80bc5470de66ec645ae3fafe539425c5a
Denis Vasilev <n1hron@yandex.com>
2026-08-12 02:17:33 +0300

    Adjust imports rules

--- src/n1hron/imports/rules.ts
+++ src/n1hron/imports/rules.ts
@@ -4,8 +4,9 @@ export const rules: ImportsRules = {
   "import-x/export": "error",
   "import-x/no-empty-named-blocks": "error",
   "import-x/no-extraneous-dependencies": "error",
-  "import-x/first": "error",
-  "import-x/newline-after-import": "error",
-  "import-x/consistent-type-specifier-style": ["error", "prefer-top-level"],
-  "import-x/no-duplicates": ["error", { considerQueryString: true }],
+  "import-x/no-duplicates": "error",
+
+  "import-x/first": "warn",
+  "import-x/newline-after-import": "warn",
+  "import-x/consistent-type-specifier-style": ["warn", "prefer-top-level"],
 };
