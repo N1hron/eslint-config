22ea4c9ac519a1cc1b2618502f5261c100710111
Denis Vasilev <n1hron@yandex.com>
2026-08-12 00:48:14 +0300

    Add react dom rules

--- src/n1hron/react/dom/rules.ts
+++ src/n1hron/react/dom/rules.ts
@@ -1,3 +1,21 @@
 import type { ReactDomRules } from "./types.gen";
 
-export const rules: ReactDomRules = {};
+export const rules: ReactDomRules = {
+  "react-dom/no-dangerously-set-innerhtml-with-children": "error",
+  "react-dom/no-find-dom-node": "error",
+  "react-dom/no-flush-sync": "error",
+  "react-dom/no-hydrate": "error",
+  "react-dom/no-render": "error",
+  "react-dom/no-render-return-value": "error",
+  "react-dom/no-use-form-state": "error",
+  "react-dom/no-void-elements-with-children": "error",
+
+  "react-dom/no-dangerously-set-innerhtml": "warn",
+  "react-dom/no-missing-button-type": "warn",
+  "react-dom/no-missing-iframe-sandbox": "warn",
+  "react-dom/no-script-url": "warn",
+  "react-dom/no-string-style-prop": "warn",
+  "react-dom/no-unknown-property": "warn",
+  "react-dom/no-unsafe-iframe-sandbox": "warn",
+  "react-dom/no-unsafe-target-blank": "warn",
+};
