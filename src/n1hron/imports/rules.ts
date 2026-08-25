2f5a39489395845b3c2c6cbe975fd9891b0badb9
Denis Vasilev <n1hron@yandex.com>
2026-08-25 04:00:22 +0300

    Edit imports rules

--- src/n1hron/imports/rules.ts
+++ src/n1hron/imports/rules.ts
@@ -1,15 +1,15 @@
 import type { ImportsRules } from "./types.gen";
 
 export const rules: ImportsRules = {
-  "import-x/export": "error",
-  "import-x/no-empty-named-blocks": "error",
-  "import-x/no-extraneous-dependencies": "error",
-  "import-x/no-self-import": "error",
-  "import-x/named": "error",
-  "import-x/default": "error",
+  "import-x/default": "error", // Importing unexisting default import is forbidden
+  "import-x/export": "error", // All exports must be valid
+  "import-x/named": "error", // Importing unexisting named imports is forbidden
+  "import-x/no-extraneous-dependencies": "error", // Importing external extraneous may cause problems
 
-  "import-x/first": "warn",
-  "import-x/no-duplicates": "warn",
-  "import-x/newline-after-import": "warn",
   "import-x/consistent-type-specifier-style": ["warn", "prefer-top-level"],
+  "import-x/first": "warn",
+  "import-x/newline-after-import": "warn",
+  "import-x/no-duplicates": "warn",
+  "import-x/no-empty-named-blocks": "warn",
+  "import-x/no-self-import": "warn",
 };
