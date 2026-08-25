422e2404e2aa50dc3dc720e9234b3595d9c777a3
Denis Vasilev <n1hron@yandex.com>
2026-08-25 03:44:06 +0300

    Edit react/dom rules

--- src/n1hron/react/dom/rules.ts
+++ src/n1hron/react/dom/rules.ts
@@ -1,16 +1,16 @@
 import type { ReactDomRules } from "./types.gen";
 
 export const rules: ReactDomRules = {
-  "react-dom/no-dangerously-set-innerhtml-with-children": "error",
-  "react-dom/no-find-dom-node": "error",
-  "react-dom/no-flush-sync": "error",
-  "react-dom/no-hydrate": "error",
-  "react-dom/no-render": "error",
-  "react-dom/no-render-return-value": "error",
-  "react-dom/no-use-form-state": "error",
-  "react-dom/no-void-elements-with-children": "error",
+  "react-dom/no-find-dom-node": "error", // findDOMNode is deprecated and must not be used
+  "react-dom/no-hydrate": "error", // ReactDOM.hydrate() is deprecated in favor of hydrateRoot()
+  "react-dom/no-render": "error", // ReactDOM.render() is deprecated in favor of createRoot(node).render()
+  "react-dom/no-render-return-value": "error", // Crucial for react to work correctly
+  "react-dom/no-use-form-state": "error", // useFormState is deprecated in favor of useActionState
+  "react-dom/no-void-elements-with-children": "error", // Void HTML elements can't have children
 
   "react-dom/no-dangerously-set-innerhtml": "warn",
+  "react-dom/no-dangerously-set-innerhtml-with-children": "warn",
+  "react-dom/no-flush-sync": "warn",
   "react-dom/no-missing-button-type": "warn",
   "react-dom/no-missing-iframe-sandbox": "warn",
   "react-dom/no-script-url": "warn",
